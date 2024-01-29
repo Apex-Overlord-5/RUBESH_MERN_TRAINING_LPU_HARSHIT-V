85fec40711834968bd487f8c75aae598c1de75a3
Apex-Overlord-5 <114309468+Apex-Overlord-5@users.noreply.github.com>
2024-01-29 10:56:03 +0530

    Class 2 Assignment

--- Git Commands - Class 2.sh
+++ Git Commands - Class 2.sh
@@ -1,3 +1,5 @@
 git commit -n "Initial Commit"
 git config --global user. name
 git config user.name "Rubesh LPU 12115752" --replace-all
+git config user.name
+git config --global user-mail "raman.rubesh@gmail.com"
