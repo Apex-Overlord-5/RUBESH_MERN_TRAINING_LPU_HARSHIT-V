8573a53c40dc448e603b72888f10dfc22564936a
Apex-Overlord-5 <114309468+Apex-Overlord-5@users.noreply.github.com>
2024-02-07 19:43:46 +0530

    new

--- Rubesh Practical Assesment 1 Morning/E-commerce-Rubesh/src/App.jsx
+++ Rubesh Practical Assesment 1 Morning/E-commerce-Rubesh/src/App.jsx
@@ -4,10 +4,10 @@ import Product from './Product';
 
 const products = [
 
-  { id: 1, title: 'Sony Walkman 2300', description: 'Sony WalkMan 2300', price: "2300", image: 'C:\\MERN RUBESH LPU WINTER PEP\\Rubesh Practical Assesment 1 Morning\\E-commerce-Rubesh\\src\\Images\\Sony.png' },
-  { id: 2, title: 'I Phone 15', description: 'I Phone 15 is a 17th geneation samrtphone developed under apple', price: "78000", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
-  { id: 3, title: 'Oxford Dictionary', description: 'Oxford Dictionary gives you a GIST of all the newly added words and common Dictionary', price: "4000", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
-  { id: 4, title: 'Pilot V5', description: 'The chiseled tip allows you to give a good crispy sharp handwriting', price: "70", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
+  { id: 1, title: 'Sony Walkman 2300', description: 'Sony WalkMan 2300', price: " 2300", image: 'C:\\MERN RUBESH LPU WINTER PEP\\Rubesh Practical Assesment 1 Morning\\E-commerce-Rubesh\\src\\Images\\Sony.png' },
+  { id: 2, title: 'I Phone 15', description: 'I Phone 15 is a 17th geneation samrtphone developed under apple', price: " 78000", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
+  { id: 3, title: 'Oxford Dictionary', description: 'Oxford Dictionary gives you a GIST of all the newly added words and common Dictionary', price: " 4000", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
+  { id: 4, title: 'Pilot V5', description: 'The chiseled tip allows you to give a good crispy sharp handwriting', price: " 70", image: 'https://newatlas.com/mobile-technology/iphone-15-pro-max/' },
 ];
 
 const App = () => {
